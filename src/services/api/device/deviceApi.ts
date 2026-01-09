368d0b9792dac25a1a233ed91f965e7b28cd1890
Everts Zeilins <evertszeilins@Evertss-MacBook-Pro.local>
2026-01-09 19:00:13 +0200

    endpoint fixes

--- src/services/api/device/deviceApi.ts
+++ src/services/api/device/deviceApi.ts
@@ -4,7 +4,7 @@ import { parseDevicesResponse } from "./deviceSchema";
 import { axiosInstance } from "../../../lib/axios-instance";
 
 export async function fetchDevices(): Promise<DevicesResponse> {
-	const response = await axiosInstance.get<DevicesResponse>("/public2.json");
+	const response = await axiosInstance.get<DevicesResponse>("/public.json");
 
 	return parseDevicesResponse(response.data);
 }
